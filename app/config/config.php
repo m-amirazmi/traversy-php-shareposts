9d5f5acb72a75883f63060738c290212c896a404
Muhamad Amir <m.amirazmi@hotmail.com>
2020-05-28 21:59:13 +0800

    Initial setup on config

---
+++ app/config/config.php
@@ -0,0 +1,12 @@
+<?php
+	// DB Params
+	define("DB_HOST", "localhost");
+	define("DB_USER", "root");
+	define("DB_PASS", "root");
+	define("DB_NAME", "traversy_sharepost");
+	// App Root
+	define("APPROOT", dirname(dirname(__FILE__))); 
+	// URL Root
+	define("URLROOT", "http://localhost/sharepost");
+	// Site Name
+	define("SITENAME", "SharePosts");
